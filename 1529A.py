cf2f197025e793f57ef563eacce2fccd7f62f199
Ruimas-UTI <allpraisethelord@gmail.com>
2021-06-19 03:02:59 +0600

    caveat overcomming

--- 1529A.py
+++ 1529A.py
@@ -1,15 +1,17 @@
-testcase = int(input(''))
 def worker(arr):
-    iterator = 0
-    while iterator < len(arr):
-        pass
-    iterator += 1
-    if iterator <= -1:
-        return 0
-    else: return iterator
+    summation = sum(arr)
+    average = summation / len(arr)
+    new_arr = [item for item in arr if item < average]
+    print (new_arr)
+    # if iterator <= -1:
+    #     return 0
+    # else: return iterator
+testcase = int(input(''))
 for i in range(testcase):
     ##length = int(input(''))
     ##No use of length
     charm = input('')
     arr = [char for char in charm.split()]
     case = list(map(lambda x: int(x, 10) , arr))
+    worker(case)
+
