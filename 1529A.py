d05db11cd23595d3e0584946dd1dd92b2f19d647
Ruimas-UTI <allpraisethelord@gmail.com>
2021-06-20 02:43:59 +0600

    stuck

--- 1529A.py
+++ 1529A.py
@@ -1,12 +1,20 @@
+import random
+def sequenceMaker(arr):
+    counter = 0
+    while len(arr) - counter > 0:
+        subseq = random.sample(arr,len(arr)- counter)
+        worker(subseq)
+        counter += 1
 def worker(arr):
     answer = 0
+    print(f'this is before {arr}')
     while len(arr) != 0:
         summation = sum(arr)
         average = summation / len(arr)
         arr = [item for item in arr if item < average]
+        print(f'this is after {arr}')
         if len(arr) > 0:
             answer += 1
-    print (answer)
 testcase = int(input(''))
 for i in range(testcase):
     ##length = int(input(''))
@@ -14,5 +22,5 @@ for i in range(testcase):
     charm = input('')
     arr = [char for char in charm.split()]
     case = list(map(lambda x: int(x, 10) , arr))
-    worker(case)
+    sequenceMaker(case)
 
