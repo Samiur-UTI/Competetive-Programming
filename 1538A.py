373738b1c943de0fe1e0e841af5f19934baa54fb
Ruimas-UTI <allpraisethelord@gmail.com>
2021-06-21 02:26:01 +0600

    almost done

--- 1538A.py
+++ 1538A.py
@@ -7,12 +7,27 @@ for i in range(testcase):
     if int(array_length) == len(arr):
         max_distance_from_start = case.index(max(case)) + 1
         min_distance_from_start = case.index(min(case)) + 1
-        start_pair = (max_distance_from_start,min_distance_from_start)
+        difference = max_distance_from_start - min_distance_from_start
         max_distance_from_end = len(arr) - max_distance_from_start
         min_distance_from_end = len(arr) - min_distance_from_start
-        end_pair = (max_distance_from_end,min_distance_from_end)
-        if (sum(start_pair) > sum(end_pair)):
-            print(max(end_pair))
-        elif(sum(end_pair) > sum(start_pair)):
-            print(max(start_pair))
-        else: print(max(start_pair))        
+        if max_distance_from_start >= max_distance_from_end:
+            if min_distance_from_start >= min_distance_from_end:
+                lowest = [max_distance_from_end, min_distance_from_end]
+                print(max(lowest))
+        elif max_distance_from_end >= max_distance_from_start:
+            if min_distance_from_end >= min_distance_from_start:
+                lowest = [max_distance_from_start, min_distance_from_start]
+                print(max(lowest))
+        else:
+            all_distance = [max_distance_from_start, min_distance_from_start,max_distance_from_end,min_distance_from_end]
+            ans1 = min(all_distance)
+            index1 = all_distance.index(ans1)
+            all_distance.pop(index1)
+            ans2 = min(all_distance)
+            answer = ans1 + ans2
+            print(answer)
+                    
+        
+        
+
+## If lowest two are in the same set then the greater number in that set is the answer, if the lowest two are in different sets then their sum is the answer
