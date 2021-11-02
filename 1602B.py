c7ff03bf1addb78335520265319252e887da5ffb
Samiur-UTI <samiur@sharetrip.net>
2021-11-02 22:08:37 +0600

    stuck again

--- 1602B.py
+++ 1602B.py
@@ -1,22 +1,31 @@
 t = int(input(''))
+length = int(input(''))
 for x in range(t):
     n = input()
     arr = [int(char) for char in n.split()]
-    step = int(input())
-    index = int(input())
-    # take input of the index and the nth step of transform
-    o = {}
-    p = []
-    for h in range(step):
-        for i in arr:
-            x = arr.count(i)
-            o[i] = x
-        print(o)
-        for j in range(len(arr)):
-            if(arr[j] in o.keys()):
-                arr[j] = o[arr[j]]
-        p.append(tuple(arr))
-    print(p)
+    if(len(arr) == length):
+        step = int(input())
+        o = {}
+        p = []
+        B = []
+        for h in range(step):
+            _q = input()
+            q_i = [int(char) for char in _q.split()]
+            B.append(q_i)
+            for item in B:
+                if(item[1] == 0):
+                    p.append(arr[item[0]-1])
+                else:
+                    for i in range(item[1]):
+                        for i in arr:
+                            x = arr.count(i)
+                            o[i] = x
+                        for j in range(len(arr)):
+                            if(arr[j] in o.keys()):
+                                arr[j] = o[arr[j]]
+        p.append(arr[item[0]-1])
+for item in p:
+    print(f"result {item}")
         # b = [o[key] if l == int(key) else l for l in arr]     
     
     
